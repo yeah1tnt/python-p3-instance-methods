6994ae2d62d19b2b143c6fdea5fc85b4972d8dbc
Tong Tang <pichip22@hotmail.com>
2023-07-12 10:34:30 -0400

    completed

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,12 @@
 class Person:
     # Class body goes here
 
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking.")
+    
+    
     #Instance method definition
     pass
